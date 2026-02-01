d01e8c3fa790c6dd3ed757c0695427665988ebb6
itschool <itschool@tou.edu.kz>
2026-02-01 11:33:33 +0500

    update

--- src/views/EmodjiView.vue
+++ src/views/EmodjiView.vue
@@ -9,6 +9,7 @@
         placeholder="Введите название фильма">
     <button
         @click="sendFilm"
+        :disabled="isLoading"
     >
         Отправить
     </button>
@@ -22,26 +23,38 @@
         data() {
             return {
                 film: '',
-                result: ''
+                result: '',
+                isLoading: false
+            }
+        },
+        computed: {
+            prompt() {
+                return `Напиши 3 emodji, которые ассоциируются с фильмом "${this.film}". Напиши только emodji, без объяснений и всего лишнего.`
             }
         },
         methods: {
             async sendFilm() {
-                const response = await axios.post(
-                    'https://generativelanguage.googleapis.com/v1beta/models/gemini-3-flash-preview:generateContent?key=AIzaSyDKisokgDj4HheDtGjAdoEoZIucnaqJUkE',
-                    {
-                        "contents": [
-                            {
-                                "parts": [
-                                    {
-                                        "text": `Напиши 3 emodji, которые ассоциируются с фильмом "${this.film}". Напиши только emodji, без объяснений и всего лишнего.`
-                                    }
-                                ]
-                            }
-                        ]
-                    }
-                )
-                this.result = response.data.candidates[0].content.parts[0].text
+                this.isLoading = true;
+                try {
+                    const response = await axios.post(
+                        'https://generativelanguage.googleapis.com/v1beta/models/gemini-3-flash-preview:generateContent?key=AIzaSyDKisokgDj4HheDtGjAdoEoZIucnaqJUkE',
+                        {
+                            "contents": [
+                                {
+                                    "parts": [
+                                        {
+                                            "text": this.prompt
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    )
+                    this.result = response.data.candidates[0].content.parts[0].text
+                } catch(error) {
+                    alert(error)
+                }
+                this.isLoading = false;
             }
         }
     }
